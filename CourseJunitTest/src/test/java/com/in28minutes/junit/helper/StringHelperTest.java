7927995c40b75a949c44c90666a6272b4ccf5ecc
PJ7890 <priyanka.sjkv@gmail.com>
2022-12-05 17:58:38 +0530

    Refactoring Your First Junit Test, StringHelper Object and Method Name

--- CourseJunitTest/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ CourseJunitTest/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -14,34 +14,36 @@ public class StringHelperTest {
 		//AACD=>CD ACD=>CD CDEF=>CDEF CDAA=>CDAA
 		assertEquals(expected,actual);
 	} */
-	
 	//I actually created a lot of variables to really make it easy for you to understand.
 	//use InlineVariables, to do that-> right click on the varaibles-> (ex: actual) -> Refactor -> Inline
 	
+	StringHelper helper = new StringHelper();
+	//in all methods we have given the StringHelper object . so instead of that i created its instance here
+	
 	@Test
-	public void test1() {
-		StringHelper helper = new StringHelper();
+	public void testTruncateAInFirst2Positions1() {
+		
 		//AACD=>CD ACD=>CD CDEF=>CDEF CDAA=>CDAA
 		assertEquals("CD",helper.truncateAInFirst2Positions("AACD"));
 	} 
 	
 	@Test
-	public void test2() {
-		StringHelper helper = new StringHelper();
+	public void testTruncateAInFirst2Positions2() {
+		
 		//AACD=>CD ACD=>CD CDEF=>CDEF CDAA=>CDAA
 		assertEquals("CD",helper.truncateAInFirst2Positions("ACD"));
 	} 
 	
 	@Test
-	public void test3() {
-		StringHelper helper = new StringHelper();
+	public void testTruncateAInFirst2Positions3() {
+		
 		//AACD=>CD ACD=>CD CDEF=>CDEF CDAA=>CDAA
 		assertEquals("CDEF",helper.truncateAInFirst2Positions("CDEF"));
 	} 
 	
 	@Test
-	public void test4() {
-		StringHelper helper = new StringHelper();
+	public void testTruncateAInFirst2Positions4() {
+		
 		//AACD=>CD ACD=>CD CDEF=>CDEF CDAA=>CDAA
 		assertEquals("CDAA",helper.truncateAInFirst2Positions("CDAA"));
 	} 
