8259a78f2a5b0185e34ec4a55a7e47751a792446
PJ7890 <priyanka.sjkv@gmail.com>
2022-12-12 23:23:08 +0530

    Testing Performance in JunitTests

--- CourseJunitTest/src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
+++ CourseJunitTest/src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -48,5 +48,14 @@ public class ArraysCompareTest {
 		Arrays.sort(numbers);	
 	}
 	//this is the actual format of exception
+	
+	@Test(timeout = 100)
+	public void testSort_Performance() {
+		int array[] = { 12, 13, 14};
+		for (int i=1;i<=1000000; i++) {
+			array[0] = i;
+			Arrays.sort(array);
+		}
+	}
 
 }
