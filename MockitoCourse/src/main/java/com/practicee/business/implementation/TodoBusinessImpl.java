0cf2ac950bee8cdc972cf3c8efba33e6148775b6
PJ7890 <priyanka.sjkv@gmail.com>
2023-01-16 18:36:58 +0530

    Mockito- Setting Up SUT(System Under Test)

---
+++ MockitoCourse/src/main/java/com/practicee/business/implementation/TodoBusinessImpl.java
@@ -0,0 +1,31 @@
+package com.practicee.business.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.practicee.data.api.TodoService;
+
+public class TodoBusinessImpl {
+
+	//interface
+	private TodoService todoService;
+
+	//constructor using fields
+	public TodoBusinessImpl(TodoService todoService) {
+		this.todoService = todoService;
+	}
+	
+	// this below is the method we want to write a test for future
+	public List<String> retreiveTodosRelatedToSpring(String user) {
+		List<String> filteredTodos = new ArrayList<String>();
+		List<String> todos = todoService.retreiveTodos(user);
+		
+		for(String todo : todos ) {
+			if(todo.contains("Spring")) {
+				filteredTodos.add(todo);
+			}
+		}
+		
+		return filteredTodos;
+	}
+}
--- MockitoCourse/src/test/java/com/practicee/mockito/FirstMockitoTest.java
+++ MockitoCourse/src/test/java/com/practicee/mockito/FirstMockitoTest.java
@@ -4,6 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
+//TodoService.java=> WunderList => this is an Interface => WunderList
+//public List<String> retreiveTodos(String user);
+
+//TodoBusinessImpl.java
+//public List<String> retreiveTodosRelatedToSpring(string user)
+
 public class FirstMockitoTest {
 
 	@Test
